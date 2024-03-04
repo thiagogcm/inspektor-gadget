thiagogcm/inspektor-gadget

commit 7da4e7f5ae234b52bdbf1eab9504e9a4a3bb3111

pkg/gadgets: Use BTF to check arguments number of block_rq_insert.
Checking kernel version is not enough, as upstream commit can be backported
[1, 2].
So, this commit relies on BTF to check the arguments number of block_rq_insert,
it takes inspiration from similar work in iovisor/bcc [3].

Signed-off-by: Francis Laniel <flaniel@linux.microsoft.com>
[1]: https://github.com/torvalds/linux/commit/a54895fa057c
[2]: https://github.com/amazonlinux/linux/blob/kernel-5.10.205-195.807.amzn2/block/blk-mq.c#L1812
[3]: https://github.com/iovisor/bcc/commit/952415e490bd

pkg/gadgets/profile/block-io/tracer/bpf/biolatency.bpf.c
@@ -15,10 +15,10 @@ const volatile bool targ_per_disk = false;
 const volatile bool targ_per_flag = false;
 const volatile bool targ_queued = false;
 const volatile bool targ_ms = false;
+const volatile bool insert_arg_single = true;
+const volatile bool issue_arg_single = true;
 const volatile bool filter_dev = false;
 const volatile __u32 targ_dev = 0;
-
-extern int LINUX_KERNEL_VERSION __kconfig;
 
 struct {
 	__uint(type, BPF_MAP_TYPE_CGROUP_ARRAY);
@@ -75,7 +75,7 @@ int ig_profio_ins(u64 *ctx)
 	 * from TP_PROTO(struct request_queue *q, struct request *rq)
 	 * to TP_PROTO(struct request *rq)
 	 */
-	if (LINUX_KERNEL_VERSION < KERNEL_VERSION(5, 11, 0))
+	if (!insert_arg_single)
 		return trace_rq_start((void *)ctx[1], false);
 	else
 		return trace_rq_start((void *)ctx[0], false);
@@ -92,7 +92,7 @@ int ig_profio_iss(u64 *ctx)
 	 * from TP_PROTO(struct request_queue *q, struct request *rq)
 	 * to TP_PROTO(struct request *rq)
 	 */
-	if (LINUX_KERNEL_VERSION < KERNEL_VERSION(5, 11, 0))
+	if (!issue_arg_single)
 		return trace_rq_start((void *)ctx[1], true);
 	else
 		return trace_rq_start((void *)ctx[0], true);

pkg/gadgets/profile/block-io/tracer/tracer.go
@@ -22,6 +22,7 @@ import (
 	"unsafe"
 
 	"github.com/cilium/ebpf"
+	"github.com/cilium/ebpf/btf"
 	"github.com/cilium/ebpf/link"
 
 	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
@@ -93,10 +94,61 @@ func (t *Tracer) close() {
 	t.objs.Close()
 }
 
+// Highly inspired from:
+// https://github.com/iovisor/bcc/commit/952415e490bd#diff-2a74184cac5bad5399ebad3c47180658e27cca8070ddd28874017fbfdbb4690eR243
+func getTracepointArguments(tracepoint string) ([]btf.FuncParam, error) {
+	var typedef *btf.Typedef
+
+	btfSpec, err := btf.LoadKernelSpec()
+	if err != nil {
+		return nil, fmt.Errorf("loading kernel BTF information: %w", err)
+	}
+
+	if err := btfSpec.TypeByName(tracepoint, &typedef); err != nil {
+		return nil, fmt.Errorf("searching %q: %w", tracepoint, err)
+	}
+
+	ptr, ok := typedef.Type.(*btf.Pointer)
+	if !ok {
+		return nil, fmt.Errorf("expecting btf.Pointer, got: %v", typedef.Type)
+	}
+
+	funcProto, ok := ptr.Target.(*btf.FuncProto)
+	if !ok {
+		return nil, fmt.Errorf("expecting btf.FuncProto, got: %v", ptr.Target)
+	}
+
+	return funcProto.Params, nil
+}
+
 func (t *Tracer) install() error {
 	spec, err := loadBiolatency()
 	if err != nil {
 		return fmt.Errorf("loading ebpf program: %w", err)
+	}
+
+	insertArguments, err := getTracepointArguments("btf_trace_block_rq_insert")
+	if err != nil {
+		return fmt.Errorf("getting btf_trace_block_rq_insert number of arguments: %w", err)
+	}
+
+	issueArguments, err := getTracepointArguments("btf_trace_block_rq_issue")
+	if err != nil {
+		return fmt.Errorf("getting btf_trace_block_rq_issue number of arguments: %w", err)
+	}
+
+	consts := map[string]interface{}{
+		// __DECLARE_TRACE always defines the ctx argument:
+		// https://github.com/torvalds/linux/blob/39133352cbed6626956d38ed72012f49b0421e7b/include/linux/tracepoint.h#L434
+		// And these tracepoint were modified to only have one argument in:
+		// https://github.com/torvalds/linux/commit/a54895fa057c67700270777f7661d8d3c7fda88a#diff-034de3e50b949df539bb3602057e4e9d529f3e465881560df959078cd02202e0R75
+		// So, we should test for two arguments, i.e. ctx and rq.
+		"insert_arg_single": len(insertArguments) == 2,
+		"issue_arg_single":  len(issueArguments) == 2,
+	}
+
+	if err := spec.RewriteConstants(consts); err != nil {
+		return fmt.Errorf("rewriting constants: %w", err)
 	}
 
 	if err := spec.LoadAndAssign(&t.objs, nil); err != nil {

commit b9c184365f9ea27d87e49e855f9b11fa3d2cb3b0

profile/block-io: use core type matches relocations
Instead of making relocations from the Go code, use
bpf_core_type_matches() to emit relocations of type BPF_TYPE_MATCHES.

This requires support in cilium/ebpf.

When no good type is detected, reject the bpf program with
bpf_unreachable().

Fixes: 7da4e7f5ae23 ("pkg/gadgets: Use BTF to check arguments number of block_rq_insert")

Signed-off-by: Alban Crequy <albancrequy@linux.microsoft.com>

gadgets/trace_tcpdrop/program.bpf.c
@@ -181,7 +181,9 @@ int ig_tcpdrop(struct trace_event_raw_kfree_skb *ctx)
 	struct sock *sk = BPF_CORE_READ(skb, sk);
 	int reason = ctx->reason;
 
-	// If bpf_core_enum_value fails, it will return 0 and that will not be a silent failure
+	// If bpf_core_enum_value fails, the verifier will reject the program with
+	// invalid func unknown#195896080
+	// 195896080 == 0xbad2310 reads "bad relo"
 	int reason_not_specified = bpf_core_enum_value(
 		// Maybe reason type needs to be skb_drop_reason in the event struct
 		enum skb_drop_reason, SKB_DROP_REASON_NOT_SPECIFIED);

include/gadget/core_fixes.bpf.h
@@ -8,6 +8,48 @@
 #include <bpf/bpf_core_read.h>
 
 /**
+ * commit 9fdc4273b8da ("libbpf: Fix up verifier log for unguarded failed CO-RE
+ * relos") explains the idea of BPF instruction "poisoning" for failed
+ * relocations:
+ *
+ *     While failing CO-RE relocation is expected, it is expected to be
+ *     property guarded in BPF code such that BPF verifier always eliminates
+ *     BPF instructions corresponding to such failed CO-RE relos as dead code.
+ *     In cases when user failed to take such precautions, BPF verifier
+ *     provides the best log it can:
+ *
+ *       123: (85) call unknown#195896080
+ *       invalid func unknown#195896080
+ *
+ *     Such incomprehensible log error is due to libbpf "poisoning" BPF
+ *     instruction that corresponds to failed CO-RE relocation by replacing it
+ *     with invalid `call 0xbad2310` instruction (195896080 == 0xbad2310 reads
+ *     "bad relo" if you squint hard enough).
+ *
+ * cilium/ebpf uses the same stategy with the same value (0xbad2310).
+ *
+ *     const badRelo = asm.BuiltinFunc(0xbad2310)
+ *
+ * bpf_unreachable() provides a way to trigger the bpf verifier to reject the
+ * program with the same error. This can be used in the following way:
+ *
+ *     typedef void (*btf_trace_block_rq_insert___new)(void *, struct request *);
+ *
+ *     if (bpf_core_type_matches(btf_trace_block_rq_issue___new)) {
+ *         // After commit a54895fa (v5.11-rc1)
+ *         return trace_rq_start((void *)ctx[0], true);
+ *     } else {
+ *         bpf_unreachable();
+ *     }
+ *
+ * See:
+ *     https://github.com/torvalds/linux/commit/9fdc4273b8da
+ *     https://github.com/cilium/ebpf/commit/b2df9e8f0042
+ *     https://github.com/cilium/ebpf/blob/v0.13.2/btf/core.go#L44
+ */
+static void (*bpf_unreachable)(void) = (void *) 0xbad2310;
+
+/**
  * commit 2f064a59a1 ("sched: Change task_struct::state") changes
  * the name of task_struct::state to task_struct::__state
  * see:

pkg/gadgets/profile/block-io/tracer/bpf/biolatency.bpf.c
@@ -15,8 +15,6 @@ const volatile bool targ_per_disk = false;
 const volatile bool targ_per_flag = false;
 const volatile bool targ_queued = false;
 const volatile bool targ_ms = false;
-const volatile bool insert_arg_single = true;
-const volatile bool issue_arg_single = true;
 const volatile bool filter_dev = false;
 const volatile __u32 targ_dev = 0;
 
@@ -64,21 +62,33 @@ static __always_inline int trace_rq_start(struct request *rq, int issue)
 	return 0;
 }
 
+// https://github.com/torvalds/linux/commit/a54895fa057c67700270777f7661d8d3c7fda88a
+// -       TP_PROTO(struct request_queue *q, struct request *rq),
+// +       TP_PROTO(struct request *rq),
+typedef void (*btf_trace_block_rq_insert___new)(void *, struct request *);
+typedef void (*btf_trace_block_rq_insert___old)(void *, struct request_queue *q,
+						struct request *);
+typedef void (*btf_trace_block_rq_issue___new)(void *, struct request *);
+typedef void (*btf_trace_block_rq_issue___old)(void *, struct request_queue *q,
+					       struct request *);
+
 SEC("raw_tp/block_rq_insert")
 int ig_profio_ins(u64 *ctx)
 {
 	if (filter_cg && !bpf_current_task_under_cgroup(&cgroup_map, 0))
 		return 0;
 
-	/**
-	 * commit a54895fa (v5.11-rc1) changed tracepoint argument list
-	 * from TP_PROTO(struct request_queue *q, struct request *rq)
-	 * to TP_PROTO(struct request *rq)
-	 */
-	if (!insert_arg_single)
-		return trace_rq_start((void *)ctx[1], false);
-	else
+	if (bpf_core_type_matches(btf_trace_block_rq_insert___new)) {
+		// After commit a54895fa (v5.11-rc1)
 		return trace_rq_start((void *)ctx[0], false);
+	} else if (bpf_core_type_matches(btf_trace_block_rq_insert___old)) {
+		// Before commit a54895fa (v5.11-rc1)
+		return trace_rq_start((void *)ctx[1], false);
+	} else {
+		// Couldn't detect block/block_rq_insert tracepoint
+		bpf_unreachable();
+		return 0;
+	}
 }
 
 SEC("raw_tp/block_rq_issue")
@@ -87,15 +97,17 @@ int ig_profio_iss(u64 *ctx)
 	if (filter_cg && !bpf_current_task_under_cgroup(&cgroup_map, 0))
 		return 0;
 
-	/**
-	 * commit a54895fa (v5.11-rc1) changed tracepoint argument list
-	 * from TP_PROTO(struct request_queue *q, struct request *rq)
-	 * to TP_PROTO(struct request *rq)
-	 */
-	if (!issue_arg_single)
-		return trace_rq_start((void *)ctx[1], true);
-	else
+	if (bpf_core_type_matches(btf_trace_block_rq_issue___new)) {
+		// After commit a54895fa (v5.11-rc1)
 		return trace_rq_start((void *)ctx[0], true);
+	} else if (bpf_core_type_matches(btf_trace_block_rq_issue___old)) {
+		// Before commit a54895fa (v5.11-rc1)
+		return trace_rq_start((void *)ctx[1], true);
+	} else {
+		// Couldn't detect block/block_rq_issue tracepoint
+		bpf_unreachable();
+		return 0;
+	}
 }
 
 SEC("raw_tp/block_rq_complete")

pkg/gadgets/profile/block-io/tracer/tracer.go
@@ -22,7 +22,6 @@ import (
 	"unsafe"
 
 	"github.com/cilium/ebpf"
-	"github.com/cilium/ebpf/btf"
 	"github.com/cilium/ebpf/link"
 
 	gadgetcontext "github.com/inspektor-gadget/inspektor-gadget/pkg/gadget-context"
@@ -94,61 +93,10 @@ func (t *Tracer) close() {
 	t.objs.Close()
 }
 
-// Highly inspired from:
-// https://github.com/iovisor/bcc/commit/952415e490bd#diff-2a74184cac5bad5399ebad3c47180658e27cca8070ddd28874017fbfdbb4690eR243
-func getTracepointArguments(tracepoint string) ([]btf.FuncParam, error) {
-	var typedef *btf.Typedef
-
-	btfSpec, err := btf.LoadKernelSpec()
-	if err != nil {
-		return nil, fmt.Errorf("loading kernel BTF information: %w", err)
-	}
-
-	if err := btfSpec.TypeByName(tracepoint, &typedef); err != nil {
-		return nil, fmt.Errorf("searching %q: %w", tracepoint, err)
-	}
-
-	ptr, ok := typedef.Type.(*btf.Pointer)
-	if !ok {
-		return nil, fmt.Errorf("expecting btf.Pointer, got: %v", typedef.Type)
-	}
-
-	funcProto, ok := ptr.Target.(*btf.FuncProto)
-	if !ok {
-		return nil, fmt.Errorf("expecting btf.FuncProto, got: %v", ptr.Target)
-	}
-
-	return funcProto.Params, nil
-}
-
 func (t *Tracer) install() error {
 	spec, err := loadBiolatency()
 	if err != nil {
 		return fmt.Errorf("loading ebpf program: %w", err)
-	}
-
-	insertArguments, err := getTracepointArguments("btf_trace_block_rq_insert")
-	if err != nil {
-		return fmt.Errorf("getting btf_trace_block_rq_insert number of arguments: %w", err)
-	}
-
-	issueArguments, err := getTracepointArguments("btf_trace_block_rq_issue")
-	if err != nil {
-		return fmt.Errorf("getting btf_trace_block_rq_issue number of arguments: %w", err)
-	}
-
-	consts := map[string]interface{}{
-		// __DECLARE_TRACE always defines the ctx argument:
-		// https://github.com/torvalds/linux/blob/39133352cbed6626956d38ed72012f49b0421e7b/include/linux/tracepoint.h#L434
-		// And these tracepoint were modified to only have one argument in:
-		// https://github.com/torvalds/linux/commit/a54895fa057c67700270777f7661d8d3c7fda88a#diff-034de3e50b949df539bb3602057e4e9d529f3e465881560df959078cd02202e0R75
-		// So, we should test for two arguments, i.e. ctx and rq.
-		"insert_arg_single": len(insertArguments) == 2,
-		"issue_arg_single":  len(issueArguments) == 2,
-	}
-
-	if err := spec.RewriteConstants(consts); err != nil {
-		return fmt.Errorf("rewriting constants: %w", err)
 	}
 
 	if err := spec.LoadAndAssign(&t.objs, nil); err != nil {

pkg/gadgets/trace/tcpdrop/tracer/bpf/tcpdrop.bpf.c
@@ -140,7 +140,9 @@ int ig_tcpdrop(struct trace_event_raw_kfree_skb *ctx)
 	struct sock *sk = BPF_CORE_READ(skb, sk);
 	int reason = ctx->reason;
 
-	// If bpf_core_enum_value fails, it will return 0 and that will not be a silent failure
+	// If bpf_core_enum_value fails, the verifier will reject the program with
+	// invalid func unknown#195896080
+	// 195896080 == 0xbad2310 reads "bad relo"
 	int reason_not_specified = bpf_core_enum_value(
 		enum skb_drop_reason, SKB_DROP_REASON_NOT_SPECIFIED);
 